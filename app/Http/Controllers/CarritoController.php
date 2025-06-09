86c2b9925ddf3fff36f14fd7adb51928d282c8bc
Doris Guaman <karyto743@gmail.com>
2025-06-09 15:18:51 -0500

    espacio

--- app/Http/Controllers/CarritoController.php
+++ app/Http/Controllers/CarritoController.php
@@ -84,7 +84,8 @@ class CarritoController extends Controller
             ->where('id_cliente', $id_cliente)
             ->whereHas('cliente.ServicePay', function ($query) {
                 $query->where('state', 1)
-                    ->where('deleted', 0);
+                    ->where('deleted', 0); 
+                    
             })
             ->get();
 
